265911a1c8586e82ee6746601bd0012f87369c9e
Triw-12 <pagezy.tristan@gmail.com>
2025-03-01 15:17:59 +0100

    Remove inane condition

--- mp2i/utils/discord.py
+++ mp2i/utils/discord.py
@@ -31,7 +31,7 @@ def has_any_role(*items: str):
         # ctx.guild is None doesn't narrow ctx.author to Member
         guild = GuildWrapper(ctx.guild)
         if any(
-            guild.get_role_by_qualifier(item) is not None and guild.get_role_by_qualifier(item) in ctx.author.roles
+            guild.get_role_by_qualifier(item) in ctx.author.roles
             for item in items
         ):
             return True
